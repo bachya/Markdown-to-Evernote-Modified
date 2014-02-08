1318cec7e37e0e110ee87e22b72d30298859aa43
Aaron Bach <bachya1208@googlemail.com>
2014-02-08 13:54:57 -0700

    Updated code documentation

--- markdown-to-evernote-modified.rb
+++ markdown-to-evernote-modified.rb
@@ -1,8 +1,38 @@
 #!/usr/bin/env ruby -wKU
 # -*- coding: utf-8 -*-
 
-# Adapted from Martin Kopischke’s original “Markdown to Evernote” workflow (http://blog.timlockridge.com/blog/2013/02/03/using-textmate-and-markdown-with-evernote-configuring-a-workflow/)
-# Aaron Bach 2014 – License: Creative Commons Attribution Share-Alike (CC BY-SA) 3.0 Unported (http://creativecommons.org/licenses/by-sa/3.0/)
+#-------------------------------------------------------------------------------------------------------------
+#  Markdown to Evernote (Modified)
+#
+#  A modification of Tim Lockridge's Markdown to Evernote Textmate bundle that allows for Evernote checkboxes
+#  to be interpreted correctly.
+#
+#  Markdown to Evernote copyright Tim Lockridge 2014 <http://timlockridge.com>
+#
+#  Copyright (c) 2014
+#  Aaron Bach <bachya1208@gmail.com>
+#  
+#  Permission is hereby granted, free of charge, to any person
+#  obtaining a copy of this software and associated documentation
+#  files (the "Software"), to deal in the Software without
+#  restriction, including without limitation the rights to use,
+#  copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the
+#  Software is furnished to do so, subject to the following
+#  conditions:
+#  
+#  The above copyright notice and this permission notice shall be
+#  included in all copies or substantial portions of the Software.
+#  
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#  OTHER DEALINGS IN THE SOFTWARE.
+#-------------------------------------------------------------------------------------------------------------
 
 # Markdown executable path
 # – edit to match your install location if non-default
